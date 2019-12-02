8380091242ed24eeef7321ece117b1dc698ac9e0
ryananthonydiaz <email@ryananthonydiaz.com>
2019-12-02 09:40:57 -0800

    Placed routes in its own routes folder Brought routes/bootcamps.js into server.js Mounted base route to /routes/bootcamps.js MASTER

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const dotenv = require('dotenv');
 
+// Routing file './routes/bootcamps.js
+const bootcamps = require('./routes/bootcamps')
+
 // Load env vars
 dotenv.config({ path: './config/config.env' });
 
 const app = express();
+
+// Mount router to a specific file in this case './routes/bootcamps.js
+app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
