f9438eaa12a97f1efbeee3564c5cd0976cb53cd8
ryananthonydiaz <email@ryananthonydiaz.com>
2020-01-05 20:12:16 -0800

    Async middleware added
    node installed/updated
    
    slugify installed
    
    console.log() removed from Error.js
    
    middleware brought into /modles/Bootcamps.js
    
    MASTER

--- middleware/Error.js
+++ middleware/Error.js
@@ -4,8 +4,6 @@ const errorHandler = (err, req, res, next) => {
   let error = { ...err };
 
   error.message = err.message;
-  // Log to console for dev
-  console.log(error);
 
   if (err.name === 'CastError') {
     const message = `Resource not found with id of ${error.value}`;
